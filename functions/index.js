38da9a9b35bb219b3110a9bd4a1049f02d6e6213
Jonahw21 <jjwrubleski21@gmail.com>
2020-02-06 19:09:26 -0600

    Return more video info

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,8 @@
 // });
 
 const ytdl = require('ytdl-core');
+const fs = require('fs');
+const cors = require('cors')({origin: true});
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
@@ -46,24 +48,65 @@ exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
   return snapshot.ref.parent.child('uppercase').set(uppercase);
 });
 
+exports.videoDownload = functions.https.onRequest((req, res) => {
+  var URL = req.query.URL;
+  console.log(URL);
+  // var info = ytdl.getInfo(URL);
+  // ytdl.downloadFromInfo(info);
+
+  // var stream = ytdl(URL)
+
+  
+  // res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+  // ytdl(URL, {format: 'mp4', quality: 'lowest'}).pipe(res);
+  // res.send();
+
+  var links = []
+  ytdl(URL, {format: 'mp4', quality: 'lowest'}).on(err, err => {
+    err.formats
+  })
+
+  ytdl(URL, {format: 'mp4', quality: 'lowest'}, function(err, format) {
+    if (err) {res.send('the url is invalid please try again...');}
+    console.log((format.formats).length);//i am getting value... in console but not outside of the function.. 
+    //this object contains all the download links  
+    for( var i= 0, len = (format.formats).length; i < len; i++) {
+      var el = format.formats[i].container;
+      if ( el === 'mp4') {
+        console.log(format.formats[i]);
+        var download_url = format.formats[i];
+        //this push will store the all links in 'links' object..
+        links.push(download_url);
+      }
+    }
+  });
+  console.log(links);
+  res.send(links);
+
+
+  // var stream = ytdl(URL, {quality: 'lowest', filter: format => format.container === 'mp4'}).  .on("response", response => {
+  //   // If you want to set size of file in header
+  //   res.setHeader("content-length", response.headers["content-length"]);
+  // })
+  // .pipe(res);
+  // res.download()
+  // res.send(stream);
+})
+
 exports.videoInfo = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
     var URL = req.query.URL;
     console.log(URL);
 
     ytdl.getInfo(URL, (err, info) => {
       if (err) throw err;
-      console.log(info.title);
-      console.log(info.thumbnail_url);
-      console.log(info.video_id);
-      console.log(info.iurlhq);
-      console.log(info.iurlmq);
-      console.log(info.iurlsd);
-      console.log(info.player_response.videoDetails.thumbnail.thumbnails[info.player_response.videoDetails.thumbnail.thumbnails.length - 1].url);
       response = {
         title: info.title,
         id: info.video_id,
         thumbnail: info.player_response.videoDetails.thumbnail.thumbnails[info.player_response.videoDetails.thumbnail.thumbnails.length - 1].url,
-        backup_thumbnail: `https://img.youtube.com/vi/${info.video_id}/maxresdefault.jpg`
+        backup_thumbnail: `https://img.youtube.com/vi/${info.video_id}/maxresdefault.jpg`,
+        video_length: info.length_seconds,
+        uploader: info.author
       }
       console.log(response);
       res.send(
@@ -71,5 +114,5 @@ exports.videoInfo = functions.https.onRequest((req, res) => {
       );
     
     })
-    //Thumbnail url can also be found at https://img.youtube.com/vi/${videoID}/maxresdefault.jpg
+})
 })
